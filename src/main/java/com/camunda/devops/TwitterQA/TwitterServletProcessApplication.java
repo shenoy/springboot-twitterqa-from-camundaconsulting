e1cf3824370240c3f71b559fc96523215dca31d8
rshenoy-pc\rshenoy <rshenoy@bp-3.com>
2023-03-13 14:31:57 +0000

    war can be deployed but wildfly shows 403

---
+++ src/main/java/com/camunda/devops/TwitterQA/TwitterServletProcessApplication.java
@@ -0,0 +1,9 @@
+package com.camunda.devops.TwitterQA;
+
+import org.camunda.bpm.application.ProcessApplication;
+import org.camunda.bpm.application.impl.ServletProcessApplication;
+
+@ProcessApplication
+public class TwitterServletProcessApplication extends ServletProcessApplication {
+
+}
